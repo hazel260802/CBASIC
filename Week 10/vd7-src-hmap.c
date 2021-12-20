7605e488abb9b1a9430a7dae21c1f0f86cd25ce9
hazel260802 <87821296+hazel260802@users.noreply.github.com>
2021-12-20 10:52:31 +0700

    Bài thực hành tuần 10

---
+++ Week 10/vd7-src-hmap.c
@@ -0,0 +1,59 @@
+#include "cgen.h"
+#include "ext/io.h"
+#include "vd7-lisa.h"
+
+struct word_tf {
+  char *word;
+  long tf;
+};
+
+#define TF(p)  (((struct word_tf*)p)->tf)
+int word_tf_inc_cmp(const void *p1, const void *p2) {
+  return TF(p2) - TF(p1);
+}
+
+#define N 10
+
+void b1_parse(FILE *inp, hmap_t words) {
+  char buff[1024];
+  while (fscanf(inp, "%s", buff) == 1) {
+    gtype *value = hmap_value(words, gtype_s(buff));
+    if (value) {
+      ++(value->i);
+    } else {
+      hmap_insert(words, gtype_s(strdup(buff)), gtype_i(1));
+    }
+  }
+}
+
+struct word_tf *b2_order_by_tf(hmap_t words) {
+  long sz = hmap_nnodes(words);
+  printf("Số lượng từ duy nhất = %ld\n", sz);
+  struct word_tf *a = malloc(sizeof(struct word_tf) * sz);
+  long idx = 0;
+  hmap_traverse(key, value, words) {
+    a[idx].word = key->s;
+    a[idx].tf = value->i;
+    ++idx;
+  }
+  qsort(a, idx, sizeof(struct word_tf), word_tf_inc_cmp);
+  return a;
+}
+
+void b3_output(struct word_tf *a, long n) {
+  for (long i = 0; i < n; ++i) {
+    printf("%s: %ld\n", a[i].word, a[i].tf);
+  }
+}
+
+void stop_words(const char *fname) {
+  FILE *inp = fopen(fname, "r");
+  hmap_t words = hmap_create(gtype_hash_s, gtype_cmp_s, gtype_free_s, NULL);
+  b1_parse(inp, words);
+  fclose(inp);
+  struct word_tf *a = b2_order_by_tf(words);
+  b3_output(a, N);
+
+  hmap_free(words);
+  free(a);
+}
